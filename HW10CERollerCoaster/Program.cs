5856524c28681c29c9e8f0f6934b393fa4cf30ed
dmeraz <darin.meraz@gmail.com>
2016-09-22 20:14:25 -0400

    first commit

---
+++ HW10CERollerCoaster/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HW10CERollerCoaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader hamlet = new StreamReader("..\\..\\..\\Hamlet.txt");
+            string line = hamlet.ReadLine();
+
+        }
+    }
+}
